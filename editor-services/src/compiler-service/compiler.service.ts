2491ad582b9da3c6b7110f8a6d340721b1590b09
Mohammad Hassany <114956341+moh70pitt@users.noreply.github.com>
2023-04-05 12:00:38 -0400

    generate sql queries, downoad as zip

--- editor-services/src/compiler-service/compiler.service.ts
+++ editor-services/src/compiler-service/compiler.service.ts
@@ -1,141 +1,172 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
-    ensureDirSync, writeJsonSync, statSync,
-    existsSync, rmSync, readJsonSync, readdirSync
+  ensureDirSync, writeJsonSync, statSync,
+  existsSync, rmSync, readJsonSync, readdirSync, createWriteStream
 } from 'fs-extra';
 import { v4 as uuid4 } from 'uuid';
 import { exec, cd } from 'shelljs';
 import { SourcesService } from '../sources-service/sources.service';
 import { ActivitiesService } from '../activities-service/activities.service';
+import { create } from 'archiver';
 
 @Injectable()
 export class CompilerService {
 
-    constructor(
-        private sources: SourcesService,
-        private activities: ActivitiesService,
-        private config: ConfigService,
-    ) {
-        ensureDirSync(this.root);
+  constructor(
+    private sources: SourcesService,
+    private activities: ActivitiesService,
+    private config: ConfigService,
+  ) {
+    ensureDirSync(this.root);
+  }
+
+  get root() {
+    return `${this.config.get('STORAGE')}/compiler`;
+  }
+
+  exists(id: string) {
+    const outputsDir = `${this.root}/${id}/outputs`;
+    return existsSync(outputsDir) && readdirSync(outputsDir).length > 0;
+  }
+
+  stat(id: string) {
+    if (this.exists(id)) {
+      const outputsDir = `${this.root}/${id}/outputs`;
+      const outputs = readdirSync(outputsDir);
+      const stat = statSync(`${outputsDir}/${outputs[0]}`);
+      return {
+        size: stat.size,
+        lastTimeModified: stat.mtimeMs,
+      };
     }
 
-    get root() {
-        return this.config.get('STORAGE') + '/compiler';
-    }
+    return null;
+  }
 
-    exists(id: string) {
-        const outputsDir = this.root + '/' + id + '/outputs';
-        return existsSync(outputsDir) && readdirSync(outputsDir).length > 0;
-    }
+  path(id: string) {
+    const outputsDir = `${this.root}/${id}/outputs`;
+    const outputs = readdirSync(outputsDir);
+    return outputs.length ? `${outputsDir}/${outputs[0]}` : null;
+  }
 
-    stat(id: string) {
-        if (this.exists(id)) {
-            const outputsDir = this.root + '/' + id + '/outputs';
-            const outputs = readdirSync(outputsDir);
-            const stat = statSync(outputsDir + '/' + outputs[0]);
-            return {
-                size: stat.size,
-                lastTimeModified: stat.mtimeMs,
-            };
-        }
+  read(id: string) {
+    const path = this.path(id);
+    return path ? readJsonSync(path) : null;
+  }
 
-        return null;
-    }
+  async compile(id: any) {
+    const activity = this.activities.read(id);
+    if (activity == null)
+      return null;
 
-    path(id: string) {
-        const outputsDir = this.root + '/' + id + '/outputs';
-        const outputs = readdirSync(outputsDir);
-        return outputs.length ? outputsDir + '/' + outputs[0] : null;
-    }
+    const workspace = `${this.root}/${activity.id}`;
+    ensureDirSync(workspace);
 
-    read(id: string) {
-        const path = this.path(id);
-        return path ? readJsonSync(path) : null;
-    }
+    const inputs = `${workspace}/inputs/`;
+    ensureDirSync(inputs);
 
-    async compile(id: any) {
-        const activity = this.activities.read(id);
-        if (activity == null)
-            return null;
+    activity.items.forEach(each => {
+      const source = this.sources.read(each.item);
 
-        const workspace = this.root + '/' + activity.id;
-        ensureDirSync(workspace);
+      let lineNum = 1;
+      const lineList = (source.code || '').split('\n')
+        .map((line: string) => ({
+          id: uuid4(),
+          number: lineNum++,
+          content: line,
+          commentList: `${lineNum}` in source.lines
+            ? source.lines[`${lineNum}`].comments
+              .map(comment => comment.content)
+            : [],
+          indentLevel: this.indentLevel(line)
+        }));
 
-        const inputs = workspace + '/inputs';
-        ensureDirSync(inputs);
+      writeJsonSync(`${inputs}${source.id}`, {
+        id: source.id,
+        activityName: activity.name,
+        order: 0,
+        name: source.name,
+        goalDescription: source.description,
+        language: source.language.toUpperCase(),
+        userInput: source.userInput || '',
+        filename: source.filename,
+        lineList,
+        distractorList: (source.distractors || [])
+          .map((distractor: any) => ({
+            id: uuid4(),
+            line: {
+              id: uuid4(),
+              number: 0,
+              content: distractor.code,
+              commentList: [distractor.description],
+              indentLevel: this.indentLevel(distractor.code)
+            }
+          })),
+        blankLineList: Object.keys(source.lines || {})
+          .filter(lineNum => source.lines[lineNum].blank)
+          .map(lineNum => ({
+            id: uuid4(),
+            line: lineList[parseInt(lineNum) - 1],
+            helpList: lineList[parseInt(lineNum) - 1].commentList
+          })),
+        fullyWorkedOut: each.type == 'example'
+      });
+    });
 
-        activity.items.forEach(each => {
-            const source = this.sources.read(each.item);
+    const outputs = `${workspace}/outputs/`;
+    if (existsSync(outputs))
+      rmSync(outputs, { recursive: true });
 
-            let lineNum = 1;
-            const lineList = (source.code || '').split('\n')
-                .map((line: string) => ({
-                    id: uuid4(),
-                    number: lineNum++,
-                    content: line,
-                    commentList: `${lineNum}` in source.lines
-                        ? source.lines[`${lineNum}`].comments
-                            .map(comment => comment.content)
-                        : [],
-                    indentLevel: this.indentLevel(line)
-                }));
+    const queries = `${workspace}/queries/`;
+    if (existsSync(queries))
+      rmSync(queries, { recursive: true });
 
-            writeJsonSync(inputs + '/' + source.id, {
-                id: source.id,
-                activityName: activity.name,
-                order: 0,
-                name: source.name,
-                goalDescription: source.description,
-                language: source.language.toUpperCase(),
-                userInput: source.userInput || '',
-                filename: source.filename,
-                lineList,
-                distractorList: (source.distractors || [])
-                    .map((distractor: any) => ({
-                        id: uuid4(),
-                        line: {
-                            id: uuid4(),
-                            number: 0,
-                            content: distractor.code,
-                            commentList: [distractor.description],
-                            indentLevel: this.indentLevel(distractor.code)
-                        }
-                    })),
-                blankLineList: Object.keys(source.lines || {})
-                    .filter(lineNum => source.lines[lineNum].blank)
-                    .map(lineNum => ({
-                        id: uuid4(),
-                        line: lineList[parseInt(lineNum) - 1],
-                        helpList: lineList[parseInt(lineNum) - 1].commentList
-                    })),
-                fullyWorkedOut: each.type == 'example'
-            });
-        });
+    const PcExParserRunner = exec(`cd ${await this.config.get('COMPILER_WORKSPACE')} && ` +
+      `java -cp ${await this.config.get('COMPILER_JAR_NAME')} application.PcExParserRunner "../${inputs}" "../${outputs}"`);
+    const UMActivityQueryGenerator = exec(`cd ${await this.config.get('COMPILER_WORKSPACE')} && ` +
+      `java -cp ${await this.config.get('COMPILER_JAR_NAME')} application.UMActivityQueryGenerator "../${outputs}" "../${queries}"`);
 
-        const outputs = workspace + '/outputs';
-        if (existsSync(outputs))
-            rmSync(outputs, { recursive: true });
+    return {
+      PcExParserRunner: {
+        code: PcExParserRunner.code,
+        stdout: PcExParserRunner.stdout,
+        stderr: PcExParserRunner.stderr,
+      },
+      UMActivityQueryGenerator: {
+        code: UMActivityQueryGenerator.code,
+        stdout: UMActivityQueryGenerator.stdout,
+        stderr: UMActivityQueryGenerator.stderr,
+      }
+    };
+  }
 
+  indentLevel(line: string) {
+    line = line.replace(/^(\s+).*$/, "$1");
+    var spaces = line.length - line.replace(/[ ]/g, "").length;
+    var tabs = line.length - line.replace(/\t/g, "").length;
+    return tabs || Math.floor(spaces / 4);
+  }
 
-        const compiler = exec(
-            // navigate into COMPILER_WORKSPACE
-            `cd ${await this.config.get('COMPILER_WORKSPACE')} && ` +
-            // then run the compiler (prefix inputs/outputs path with '../' since we are one level deeper in root)
-            `java -jar ${await this.config.get('COMPILER_JAR_NAME')} "../${inputs}" "../${outputs}"`
-        );
+  async archive(id: string) {
+    const workspace = `${this.root}/${id}`;
+    if (!existsSync(workspace))
+      return null;
 
-        return {
-            code: compiler.code,
-            stdout: compiler.stdout,
-            stderr: compiler.stderr,
-        };
-    }
+    const tmpdir = `${this.config.get('STORAGE')}/tmp/`;
+    ensureDirSync(tmpdir);
 
-    indentLevel(line: string) {
-        line = line.replace(/^(\s+).*$/, "$1");
-        var spaces = line.length - line.replace(/[ ]/g, "").length;
-        var tabs = line.length - line.replace(/\t/g, "").length;
-        return tabs || Math.floor(spaces / 4);
-    }
+    const output = `${tmpdir}${id}.zip`;
+    if (existsSync(output))
+      rmSync(output);
+
+    const archive = create('zip', { zlib: { level: 9 } });
+    archive.pipe(createWriteStream(output));
+    archive.directory(`${workspace}/outputs/`, false);
+    const queries = `${workspace}/queries/`;
+    archive.directory(`${queries}${readdirSync(queries)[0]}`, false);
+    await archive.finalize();
+
+    return output;
+  }
 }
--- editor-services/src/compiler/compiler.controller.ts
+++ editor-services/src/compiler/compiler.controller.ts
@@ -1,6 +1,6 @@
 import {
-    Controller, Get, Header, NotFoundException,
-    Param, Patch, Res, StreamableFile
+  Controller, Get, Header, NotFoundException,
+  Param, Patch, Res, StreamableFile
 } from '@nestjs/common';
 import type { Response } from 'express';
 import { createReadStream } from 'fs-extra';
@@ -10,33 +10,31 @@ import { CompilerService } from 'src/compiler-service/compiler.service';
 @Controller('compiler')
 export class CompilerController {
 
-    constructor(
-        private api: CompilerService,
-        private activities: ActivitiesService,
-    ) { }
+  constructor(
+    private api: CompilerService,
+    private activities: ActivitiesService,
+  ) { }
 
-    @Patch(':id')
-    create(@Param('id') id: string) {
-        if (!this.activities.exists(id))
-            throw new NotFoundException();
+  @Patch(':id')
+  create(@Param('id') id: string) {
+    if (!this.activities.exists(id))
+      throw new NotFoundException();
 
-        this.api.compile(id);
-    }
+    this.api.compile(id);
+  }
 
-    @Get(':id')
-    // @Header('Content-Type', 'application/json')
-    // @Header('Content-Disposition', 'attachment; filename="package.json"')
-    get(@Param('id') id: string, @Res({ passthrough: true }) res: Response) {
-        if (!this.api.exists(id))
-            throw new NotFoundException();
+  @Get(':id')
+  async get(@Param('id') id: string, @Res({ passthrough: true }) res: Response) {
+    if (!this.api.exists(id))
+      throw new NotFoundException();
 
-        const activity = this.api.read(id);
-        const filename = activity.length ? activity[0].activityName : `Activity-${id}`;
+    const activity = this.api.read(id);
+    const filename = activity.length ? activity[0].activityName : `Activity-${id}`;
 
-        const file = createReadStream(this.api.path(id));
-        res.header('Content-Type', 'application/json');
-        res.header('Content-Disposition', `attachment; filename="${filename}.json"`);
+    const file = createReadStream(await this.api.archive(id));
+    res.header('Content-Type', 'application/zip');
+    res.header('Content-Disposition', `attachment; filename="${filename}.zip"`);
 
-        return new StreamableFile(file);
-    }
+    return new StreamableFile(file);
+  }
 }
