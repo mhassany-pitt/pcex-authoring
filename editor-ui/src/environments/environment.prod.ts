2ad9d666f999cf067b75a9094d642c1586f223d3
Mohammad Hassany <114956341+moh70pitt@users.noreply.github.com>
2023-12-18 14:02:28 -0500

    updated env files

--- editor-ui/src/environments/environment.prod.ts
+++ editor-ui/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
-const apiUrl = document.querySelector('base')?.href + 'api';
+export const baseHref = document.querySelector('base')?.href;
 export const environment = {
   production: true,
-  apiUrl
+  apiUrl: baseHref + 'api',
 };
+export const uiBaseHref = baseHref;
