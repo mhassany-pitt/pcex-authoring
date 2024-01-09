8e75794486557e821f61d3a45e01f7921276907b
Mohammad Hassany <114956341+moh70pitt@users.noreply.github.com>
2024-01-08 22:40:05 -0500

    show welcome message

--- editor-ui/src/app/app.component.ts
+++ editor-ui/src/app/app.component.ts
@@ -7,5 +7,14 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+
+  docUrl = 'https://docs.google.com/document/d/1biWaF23Vy7tFpXQE0WDgte3_iaaWfa1MrJxxxs7bpHg';
+
+  get welcomeMessageDismissed() { return localStorage.getItem('pcex-authoring.welcome') === 'dismissed'; }
+
   constructor(public app: AppService) { }
+
+  dismissWelcomeMessage() {
+    localStorage.setItem('pcex-authoring.welcome', 'dismissed');
+  }
 }
